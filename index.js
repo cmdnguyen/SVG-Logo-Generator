44d4b645620ffca157cf0b9917c65acdfd907861
cmdnguyen <cmdnguyen94@gmail.com>
2023-07-12 12:31:42 -0500

    Provided example SVG and put in the code in the shapes.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const { readFile, writeFile } = require('fs/promises');
-const shapes = requre("./lib/shapes.js")
+// const shapes = requre("./lib/shapes.js")
 
 
 inquirer.prompt([
@@ -8,12 +8,6 @@ inquirer.prompt([
         type: "input",
         name: "text",
         message: "Please enter your brand initials (cannot be more than 3 characters)"
-    },
-    {
-        type: "input",
-        message: "What color would you like the text to be?",
-        name: "textColor",
-        
     },
     {
         type: "list",
@@ -25,8 +19,16 @@ inquirer.prompt([
         type: "input",
         name: "shapeColor",
         message: "What color would you like the shape to be?"
+    },
+    {
+        type: "input",
+        message: "What color would you like the text to be?",
+        name: "textColor",
+        
     }
 
-]).then((response) =>
-    console.log(response))
+]).then((response) => {
+    console.log(response);
+    console.log("\nGenerated logo.svg!")})
+.catch((error) => console.log(error))
 
