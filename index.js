812e0c76d17f227f86275f9d9a1e040dd5bf8a70
cmdnguyen <cmdnguyen94@gmail.com>
2023-07-20 08:43:18 -0500

    Added comment describing the program's functionality

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+//Imports Node packages
 const inquirer = require("inquirer");
 const { writeFile } = require('fs/promises');
+
+//Imports paths for questions and generateSVG function
 const questions = require("./lib/questions.js")
 const generateSVG = require("./lib/generateSVG.js")
 
-
+//Initializes inquirer when NPM starts
 inquirer.prompt(questions)
 .then((response) => {
     console.log(response);
@@ -11,6 +14,7 @@ inquirer.prompt(questions)
     writeFile("logo.svg", svg)
     console.log("\nGenerated logo.svg!")
 })
+//Will callback an error if it occurs in the promise before
 .catch((error) => {
     console.log(error);
     console.log("\nOops! Something went wrong.")
